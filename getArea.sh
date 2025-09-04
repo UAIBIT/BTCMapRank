8bc7cb0e08e896cd744d85e93783d0b81b193d45
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 20:54:42 +0000

    ci: simplify if

--- getArea.sh
+++ getArea.sh
@@ -1,8 +1,8 @@
 for i in $(seq 1 10);
 do
     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
-    if [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]]; then
+    [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
       areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
       echo "$areaItem"
-    fi
+    }
 done
