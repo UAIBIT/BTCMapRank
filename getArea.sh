ec69fb53ec6d30b58722038c4355a95375ffd0b0
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 21:08:33 +0000

    ci: echo array into ranking items

--- getArea.sh
+++ getArea.sh
@@ -2,7 +2,7 @@ for i in $(seq 1 10);
 do
     touch "rankingItems.json"
     [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -ne 0 ]] && {
-      rankingItems.json < "[]"
+      rankingItems.json < echo "[]"
     }
     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
     [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
