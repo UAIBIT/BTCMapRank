5f373e73c1cac4d451fa07b8487164856a7e68f8
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 20:16:39 +0000

    ci: fix response and area item

--- getArea.sh
+++ getArea.sh
@@ -1,6 +1,6 @@
 for i in $(seq 1 10);
 do
     response = $(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
-    areaItem = $(echo $response | jq 'del(.tags.geojson)' | jq '.tags')
-    echo $areaItem
+    areaItem = $(echo "$response" | jq 'del(.tags.geojson)' | jq '.tags')
+    echo "$areaItem"
 done
