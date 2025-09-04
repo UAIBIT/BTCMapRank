4cc0c17fbed44e0094421dd7aa6370312991135b
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 14:43:08 +0000

    ci: gen rank File

---
+++ genRank.sh
@@ -0,0 +1,6 @@
+for i in $(seq 1 10);
+do
+    curl --location 'https://api.btcmap.org/rpc' --header 'Content-Type:application/json' --data '{"jsonrpc":"2.0","method":"get_area_dashboard","params":{"area_id":'$i'},"id":1}'
+done
+
+
