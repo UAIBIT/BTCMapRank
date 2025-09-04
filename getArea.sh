4cfe5ea4dc57ca1f0972998e4c0df73cd691d85e
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 20:13:18 +0000

    ci: get most important data from geojson item

--- getArea.sh
+++ getArea.sh
@@ -1,4 +1,6 @@
 for i in $(seq 1 10);
 do
-    curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json'
+    response = $(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
+    areaItem = $(echo $response | jq '.tags del(.tags.geojson)')
+    echo $areaItem
 done
