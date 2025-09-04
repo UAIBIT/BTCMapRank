f76c43020f85453fccc3dc1554259003c0e2ac91
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 21:04:43 +0000

    ci: add empty array if has no file

--- getArea.sh
+++ getArea.sh
@@ -1,5 +1,9 @@
 for i in $(seq 1 10);
 do
+    touch "rankingItems.json"
+    [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -ne 0 ]] && {
+      rankingItems.json < "[]"
+    }
     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
     [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
       areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
