098270887c9512800863e9b42ed09578d93e2a3d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 15:35:38 +0000

    ci: proper pass i on getting area information

--- getArea.sh
+++ getArea.sh
@@ -1,4 +1,4 @@
 for i in $(seq 1 10);
 do
-    curl -s --location 'https://api.btcmap.org/v3/areas/868' --header 'Content-Type:application/json'
+    curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json'
 done
