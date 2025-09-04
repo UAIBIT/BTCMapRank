1ab2f17d7ae9df944bad82abf5cefe0fee10e858
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-04 20:23:23 +0000

    ci: check if it has tags

--- getArea.sh
+++ getArea.sh
@@ -1,6 +1,8 @@
 for i in $(seq 1 10);
 do
     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
-    areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
-    echo "$areaItem"
+    if [[ $(echo "$response" | jq 'has("tags")') ]]; then
+      areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
+      echo "$areaItem"
+    fi
 done
