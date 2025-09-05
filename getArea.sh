77d048f2736ad57f35550ca1f060e4f650b5279a
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-05 02:41:33 +0000

    ci: sort rank

--- getArea.sh
+++ getArea.sh
@@ -1,22 +1,22 @@
 touch "rankingItems.json"
+touch "ranking.json"
 [[ $(jq -e . <<< rankingItems.json >/dev/null 2>&1; echo $?) -ne 0 ]] && {
    echo "[]" > rankingItems.json
 }
 rankingItems=$(jq "." rankingItems.json)
-for i in $(seq 0 1000);
-do
-    response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
-    areaItem='{"type":null}'
-    [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
-      areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
-      merchantCount=$(curl -s --location 'https://api.btcmap.org/rpc' --header 'Content-Type:application/json' --data '{"jsonrpc":"2.0","method":"get_area_dashboard","params":{"area_id":'$i'},"id":1}' | jq '.result.total_elements')
-      areaItemId=$(echo "$response" | jq '.id')
-      areaItem=$(echo "$areaItem" | jq '.id  = '"$areaItemId"'')
-      areaItem=$(echo "$areaItem" | jq '.merchantCount  = '"$merchantCount"'')
-      echo "$areaItem"
-    }
-   rankingItems=$(echo $rankingItems | jq '.['$i'] = '"$areaItem"'')
-done
-echo "$rankingItems" > rankingItems.json
-echo "===="
-echo "$rankingItems"
+echo "$rankingItems" | jq 'sort_by(.merchantCount)' > ranking.json
+# for i in $(seq 0 1000);
+# do
+#     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
+#     areaItem='{"type":null}'
+#     [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
+#       areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
+#       merchantCount=$(curl -s --location 'https://api.btcmap.org/rpc' --header 'Content-Type:application/json' --data '{"jsonrpc":"2.0","method":"get_area_dashboard","params":{"area_id":'$i'},"id":1}' | jq '.result.total_elements')
+#       areaItemId=$(echo "$response" | jq '.id')
+#       areaItem=$(echo "$areaItem" | jq '.id  = '"$areaItemId"'')
+#       areaItem=$(echo "$areaItem" | jq '.merchantCount  = '"$merchantCount"'')
+#       echo "$areaItem"
+#     }
+#    rankingItems=$(echo $rankingItems | jq '.['$i'] = '"$areaItem"'')
+# done
+# echo "$rankingItems" > rankingItems.json
