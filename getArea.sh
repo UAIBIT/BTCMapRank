3a11841c92ed228ff52c8ddf981fe20c30da291f
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-05 03:18:57 +0000

    ci: fix areaItem id

--- getArea.sh
+++ getArea.sh
@@ -7,7 +7,7 @@ rankingItems=$(jq "." rankingItems.json)
 for i in $(seq 0 1000);
 do
     response=$(curl -s --location 'https://api.btcmap.org/v3/areas/'$i'' --header 'Content-Type:application/json')
-    areaItem='{"id":'$1',"type":null,"merchantCount":0, "name": null}'
+    areaItem='{"id":'$i',"type":null,"merchantCount":0, "name": null}'
     [[ $(jq -e . <<<"$response" >/dev/null 2>&1; echo $?) -eq 0 && $(echo "$response" | jq 'has("tags")' -r) = "true" ]] && {
       areaItem=$(echo "$response" | jq 'del(.tags.geo_json)' | jq '.tags')
       merchantCount=$(curl -s --location 'https://api.btcmap.org/rpc' --header 'Content-Type:application/json' --data '{"jsonrpc":"2.0","method":"get_area_dashboard","params":{"area_id":'$i'},"id":1}' | jq '.result.total_elements')
