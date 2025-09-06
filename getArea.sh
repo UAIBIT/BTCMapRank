1f47de1a2c516aa1d1de85dc41f84dd049356acc
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-06 14:36:35 +0000

    ci: echo all orgs even repeating

--- getArea.sh
+++ getArea.sh
@@ -24,3 +24,6 @@ touch "rankingCommunity.json"
 jq 'sort_by(-.merchantCount)' rankingItems.json > ranking.json
 jq '.[] | select(.type == "country")' ranking.json > rankingCountry.json
 jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
+jq -c '.[]' rankingCommunity.json | while read i; do
+    echo $i | jq '.organization'
+done
