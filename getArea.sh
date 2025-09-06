0bb1d52f7b2bce7de8aaf184a69cc274ba9c3cb7
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-06 16:34:56 +0000

    ci: prope read json as raw

--- getArea.sh
+++ getArea.sh
@@ -26,7 +26,7 @@ jq 'sort_by(-.merchantCount)' rankingItems.json > ranking.json
 jq '.[] | select(.type == "country")' ranking.json > rankingCountry.json
 jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
 echo "{}" > orgs.json
-jq -c -S -R '.' rankingCommunity.json | while read i; do
+jq -c '.' rankingCommunity.json | while read -r i; do
     echo "a"
     echo "$i"
     org=$(echo "$i" | jq '.organization // "noOrg"' -r)
