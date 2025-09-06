27938321dadd637f2f3a6d4567d0ca7fed9d83a7
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-06 17:13:02 +0000

    c: file processing to tgen rank

--- genRank.sh
+++ genRank.sh
@@ -1,6 +1,11 @@
-for i in $(seq 1 10);
-do
-    curl -s --location 'https://api.btcmap.org/rpc' --header 'Content-Type:application/json' --data '{"jsonrpc":"2.0","method":"get_area_dashboard","params":{"area_id":'$i'},"id":1}' | jq '.result.total_elements'
+jq 'sort_by(-.merchantCount)' rankingItems.json > ranking.json
+jq '.[] | select(.type == "country")' ranking.json > rankingCountry.json
+jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
+echo "{}" > orgs.json
+jq -c '.' rankingCommunity.json | while read -r i; do
+    org=$(echo "$i" | jq '.organization // "noOrg"' -r)
+    merchantCount=$(echo $i | jq '.merchantCount // 0')
+    oldMerchantCount=$(jq '."'"$org"'".merchantCount // 0' orgs.json)
+    echo "$(jq '. += {"'"$org"'": {"name":"'"$org"'","merchantCount":'"$oldMerchantCount"'}}' orgs.json)" > orgs.json
+    echo "$(jq '."'"$org"'".merchantCount += '"$merchantCount"'' orgs.json)" > orgs.json
 done
-
-
--- getArea.sh
+++ getArea.sh
@@ -22,14 +22,3 @@ do
    rankingItems=$(echo $rankingItems | jq '.['$i'] = '"$areaItem"'')
 done
 echo "$rankingItems" > rankingItems.json
-jq 'sort_by(-.merchantCount)' rankingItems.json > ranking.json
-jq '.[] | select(.type == "country")' ranking.json > rankingCountry.json
-jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
-echo "{}" > orgs.json
-jq -c '.' rankingCommunity.json | while read -r i; do
-    org=$(echo "$i" | jq '.organization // "noOrg"' -r)
-    merchantCount=$(echo $i | jq '.merchantCount // 0')
-    oldMerchantCount=$(jq '."'"$org"'".merchantCount // 0' orgs.json)
-    echo "$(jq '. += {"'"$org"'": {"name":"'"$org"'","merchantCount":'"$oldMerchantCount"'}}' orgs.json)" > orgs.json
-    echo "$(jq '."'"$org"'".merchantCount += '"$merchantCount"'' orgs.json)" > orgs.json
-done
