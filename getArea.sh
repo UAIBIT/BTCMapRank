fda9982def4347f5d6198bce7c0031d818b39a46
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-06 15:58:52 +0000

    ci: proper gemerchan count from comunities

--- getArea.sh
+++ getArea.sh
@@ -28,7 +28,7 @@ jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
 echo "{}" > orgs.json
 jq -c '.' rankingCommunity.json | while read i; do
     org=$(echo $i | jq '.organization // "noOrg"' -r)
-    merchantCount=$(jq '."'"$org"'".merchantCount // 0' orgs.json)
+    merchantCount=$(echo $i | jq '.merchantCount // 0' -r)
     echo "$(jq '. += {"'"$org"'": {"name":"'"$org"'","merchantCount":'"$merchantCount"'}}' orgs.json)" > orgs.json
     echo "$(jq '."'"$org"'".merchantCount += 1' orgs.json)" > orgs.json
 done
