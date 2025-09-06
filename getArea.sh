625be507e9777a7cff13535fcc75f3a53b93a0cf
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-09-06 15:04:05 +0000

    ci: just echo org

--- getArea.sh
+++ getArea.sh
@@ -27,7 +27,7 @@ jq '.[] | select(.type == "country")' ranking.json > rankingCountry.json
 jq '.[] | select(.type == "community")' ranking.json > rankingCommunity.json
 echo "{}" > orgs.json
 jq -c '.' rankingCommunity.json | while read i; do
-    echo $i
     org=$(echo $i | jq '.organization // "noOrg"')
+    echo $org
     echo "$(jq '. += {'$org': {}}' orgs.json)" > orgs.json
 done
